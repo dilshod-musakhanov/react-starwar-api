2ee0df6df39c597dcae453c566b37cdbbba81cdf
Dilshod Musakhanov <musakhanov@yahoo.com>
2022-02-14 12:24:08 +0400

    refactoring RandomPlanet segragating responsibilites

--- src/components/random-planet/random-planet.js
+++ src/components/random-planet/random-planet.js
@@ -28,43 +28,53 @@ export default class RandomPlanet extends Component {
     }
 
     render() {
+        const {planet, loading} = this.state;
 
-        const { planet: {
-            id,
-            name,
-            population,
-            rotationPeriod,
-            diameter},
-            loading
-             }= this.state;
+        const spinner = loading ? <Spinner /> : null;
+        const content =!loading ? <PlanetView planet={planet} /> : null;
 
-        if (loading) {
-            return <Spinner />
-        }
         return (
             <div className="random-planet jumbotron rounded">
-                <img className="planet-image"
-                     src={`https://starwars-visualguide.com/assets/img/planets/${id}.jpg`}
-                />
-                <div>
-                    <h4 className="random-planet-name">{name}</h4>
-                    <ul className="list-group list-group-flush">
-                        <li className="list-group-item">
-                            <span className="term">Population</span>
-                            <span>{population}</span>
-                        </li>
-                        <li className="list-group-item">
-                            <span className="term">Rotation Period</span>
-                            <span>{rotationPeriod}</span>
-                        </li>
-                        <li className="list-group-item">
-                            <span className="term">Diameter</span>
-                            <span>{diameter}</span>
-                        </li>
-                    </ul>
-                </div>
+                {spinner}
+                {content}
             </div>
 
         );
     }
+}
+
+const PlanetView = ({planet}) => {
+
+    const {
+        id,
+        name,
+        population,
+        rotationPeriod,
+        diameter
+        }= planet;
+
+    return (
+        <React.Fragment>
+            <img className="planet-image"
+                 src={`https://starwars-visualguide.com/assets/img/planets/${id}.jpg`}
+            />
+            <div>
+                <h4 className="random-planet-name">{name}</h4>
+                <ul className="list-group list-group-flush">
+                    <li className="list-group-item">
+                        <span className="term">Population</span>
+                        <span>{population}</span>
+                    </li>
+                    <li className="list-group-item">
+                        <span className="term">Rotation Period</span>
+                        <span>{rotationPeriod}</span>
+                    </li>
+                    <li className="list-group-item">
+                        <span className="term">Diameter</span>
+                        <span>{diameter}</span>
+                    </li>
+                </ul>
+            </div>
+        </React.Fragment>
+    )
 }
