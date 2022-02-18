d9921a1e14c4c21c399b037fdfc36aaf672f26e7
Dilshod Musakhanov <musakhanov@yahoo.com>
2022-02-18 11:25:59 +0400

    example of container element

--- src/components/people-page/people-page.js
+++ src/components/people-page/people-page.js
@@ -5,6 +5,20 @@ import PersonDetails from "../person-details";
 import ErrorIndicator from "../errror-indicator";
 import SwapiService from "../../services/swapi-service";
 
+
+const Row = ({left, right}) => {
+    return (
+        <div className="row mb2">
+            <div className="col-md-6">
+                {left}
+            </div>
+            <div className="col-md-6">
+                {right}
+            </div>
+        </div>
+    )
+}
+
 export default class PeoplePage extends Component {
     swapiService = new SwapiService();
     state = {
@@ -23,19 +37,22 @@ export default class PeoplePage extends Component {
         if (this.state.hasError) {
             return <ErrorIndicator />;
         }
-        return (
-            <div className="row mb2">
-                <div className="col-md-6">
-                    <ItemList
-                        onItemSelected={this.onPersonSelected}
-                        getData={this.swapiService.getAllPeople}
-                        renderItem={(item) => `${item.name}, (${item.gender}, ${item.birthYear})`}
-                     />
 
-                </div>
-                <div className="col-md-6">
-                    <PersonDetails personId={this.state.selectedPerson} />
-                </div>
+    const itemList = (
+        <ItemList
+            onItemSelected={this.onPersonSelected}
+            getData={this.swapiService.getAllPeople}
+            renderItem={(item) => `${item.name}, (${item.gender}, ${item.birthYear})`}
+        />
+    )
+    const personDetails = (
+        <PersonDetails personId={this.state.selectedPerson} />
+    )
+        return (
+            <div>
+                <Row left={itemList} right={personDetails}/>
+                <Row left={itemList} right={personDetails}/>
+                <Row left="Good Forces" right="Evil Forces"/>
             </div>
         )
     }
